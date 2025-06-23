86047971b4b29207097c2ede70fd10b2c05a5bfb
MERLYNOMAGWA <omagwamerlyn@gmail.com>
2025-06-23 13:08:13 +0300

    Add astronaut data fetcher with file logging and timestamp

---
+++ astronauts.py
@@ -0,0 +1,31 @@
+import requests 
+from datetime import datetime
+
+def get_astronauts():
+   try:
+      response = requests.get("http://api.open-notify.org/astros.json") 
+      response.raise_for_status() 
+      data = respose.json()
+      
+      astronauts = data.get ("people", []) 
+      with open("data/iss_data.txt", "a") as file:
+         file.write(f"\n\nAstronauts in space as of {datetime.now()}:\n")
+         for astronaut in astronauts:
+            file.write(f"- {astronaut["name"]} on {astronaut["craft"]}\n")
+      
+      log_action("Astronaut data fetched successfully", True)
+      
+      print("Astronaut currently in space:")
+      for astronaut in astronauts:
+         """loops through the lists"""
+         print(f"{astronaut["name"]} ({astronaut["craft"]})")
+         
+   except Exception as e:
+      log_action(f"Failed to fetch astronauts: {e}", False)
+      print("Error fetching astronaut data.")
+      
+def log_action(message, success):
+   with open("logs.txt", "a") as log_file:
+      status = "SUCCESS" if success else "FAILURE"    
+      log_write.write(f"{datetime.now()} - {status}: {message}\n")
+   
